2a6cf2a4b89293acb3ca377acebc49dca94bf2d9
AaronOnAir <81103729+AaronOnAir@users.noreply.github.com>
2021-08-11 17:16:46 +0300

    Initial commit

---
+++ src/test/java/com/vytrack/pages/DashboardPage.java
@@ -0,0 +1,16 @@
+package com.vytrack.pages;
+
+public class DashboardPage extends BasePage {
+
+    //no need to explicitly create constructor,
+    //because it will use its parent constructor
+
+    //Constructors are not members, so they are not inherited by subclasses, but the constructor
+    //of the superclass can be invoked from the subclass
+
+
+
+
+
+
+}
